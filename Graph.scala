425b0899c26cbe08e499019ff5e04920f93cb93e
johannesgrohn <johannes.ah.grohn@gmail.com>
2025-04-02 14:24:22 +0200

    Initial commit

---
+++ Graph.scala
@@ -0,0 +1,64 @@
+import scala.collection.mutable.Queue
+import scala.collection.mutable.Set
+import scala.compiletime.ops.double
+import scala.collection.mutable.ArrayBuffer
+
+
+
+
+
+
+case class Graph(private val words: Vector[String]):
+    private val _nodes: ArrayBuffer[Node] = ArrayBuffer.empty
+
+    def pairwisePermutations(): Vector[Vector[String]] =
+        words.combinations(2)
+        .toVector
+        .map(_.permutations.toVector)
+        .flatten
+
+    
+    Map
+    
+    for (word1 <- words) yield
+        val neighbours: Set[String] = Set()
+        for (word2 <- words) do
+            if word1 != word2 && word1.substring(1).intersect(word2) == word1.substring(1) then
+                neighbours.add(word2)
+        
+    //words.combinations(2).foreach(pair => if pair(0).substring(1).forall(char => pair(1).contains(char)) then ) 
+    
+    
+    case class Node(word: String, neighbours: Set[Node])
+    
+    def findShortestPath(firstWord: Node, secondWord: Node): Unit =
+        val visited: Set[Node] = Set(firstWord)
+        val queue: Queue[Node] = Queue(firstWord)
+
+        var found: Boolean = false 
+
+        while !found || !queue.isEmpty do
+            val current: Node = queue.dequeue()
+
+            val neighbours: Set[Node] = current.neighbours
+            for neighbour <- neighbours do
+                if !visited.contains(neighbour) then
+                    visited.add(neighbour)
+                    queue.enqueue(neighbour)
+                    //pred(neighbour) <- current
+                    if neighbour.equals(secondWord) then
+                        found = true
+                        queue.clear()
+                        
+        if found then println(level) else println("Impossible")
+
+                
+                        
+
+
+
+
+
+
+
+    
