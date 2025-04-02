aca0603ef1ecba4c7871f423c798cfbd3e7dd2a5
johannesgrohn <johannes.ah.grohn@gmail.com>
2025-04-02 20:29:56 +0200

    changed bfs and nodes

--- BFS.scala
+++ BFS.scala
@@ -6,39 +6,63 @@ import scala.compiletime.ops.double
 import scala.collection.mutable.ArrayBuffer
 
 case class BFS(val nodes: Set[Node]):
-    
-    def findShortestPath(firstWord: Node, secondWord: Node): Unit =
-        val visited: Set[Node] = Set.empty
-        val queue: Queue[Node] = Queue(firstWord)
+
+    def findShortestPath(start: Node, end: Node): Unit =
+        val queue: Queue[Node] = Queue.empty
+        start.setVisited()
+        queue.enqueue(start)
 
         var found: Boolean = false 
-        var level: Int = 0
-        if firstWord == secondWord then 
-            found = true
-            
-        for nodes <- visited do 
-            visited.add(firstWord)
-            while !found && !queue.isEmpty do
-                level += 1
-                val current: Node = queue.dequeue()
-                val neighbours: Set[Node] = current.neighbours
-                for neighbour <- neighbours do
-                    if !visited.contains(neighbour) then
-                        visited.add(neighbour)
+
+        for node <- nodes do
+            node.setUnvisited()
+        
+        while !queue.isEmpty && !found do
+            val current: Node = queue.dequeue()
+            if current == end then
+                found = true
+            if !found then
+                for neighbour <- current.neighbours do 
+                    if !neighbour.visited then
+                        neighbour.setVisited()
                         queue.enqueue(neighbour)
-                        //pred(neighbour) <- current
-                        if neighbour == secondWord then
-                            found = true
+                        println(current)
+                        neighbour.pred = Some(current)
+
+
+        
+        if found then 
+            var level: Int = 0 
+            var current: Option[Node] = Some(end)
+            while !current.get.pred.isEmpty do
+                current = current.get.pred
+                level += 1
+            println(level)
+        else 
+            println("Impossible")
+    // def findShortestPath(firstWord: Node, secondWord: Node): Unit =
+    //     val visited: Set[Node] = Set.empty
+    //     val queue: Queue[Node] = Queue(firstWord)
+
+    //     var found: Boolean = false 
+    //     var level: Int = 0
+    //     if firstWord == secondWord then 
+    //         found = true
+            
+    //     for nodes <- visited do 
+    //         visited.add(firstWord)
+    //         while !found && !queue.isEmpty do
+    //             level += 1
+    //             val current: Node = queue.dequeue()
+    //             val neighbours: Set[Node] = current.neighbours
+    //             for neighbour <- neighbours do
+    //                 if !visited.contains(neighbour) then
+    //                     visited.add(neighbour)
+    //                     queue.enqueue(neighbour)
+    //                     //pred(neighbour) <- current
+    //                     if neighbour == secondWord then
+    //                         found = true
                         
-        if found then println(level) else println("Impossible")
-
-                
-                        
-
-
-
-
-
-
+    //     if found then println(level) else println("Impossible")
 
     
--- Node.scala
+++ Node.scala
@@ -2,5 +2,12 @@ package main
 
 import scala.collection.mutable.Set
 
-case class Node(val value: String):
+case class Node(val value: String, var visited: Boolean = false):
     var neighbours: Set[Node] = Set.empty
+    var pred: Option[Node] = None
+
+    def setVisited(): Unit = 
+        visited = true 
+
+    def setUnvisited(): Unit = 
+        visited = false
